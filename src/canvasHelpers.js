c891bc5dba936ce583b711f84ccbf7b408cb9d42
Michelle Van <mjtvan@gmail.com>
2019-11-02 16:58:29 +0000

    Done.

--- src/canvasHelpers.js
+++ src/canvasHelpers.js
@@ -45,7 +45,7 @@ export function toggleCycling() {
 }
 
 export function resize(type) {
-  const multiplier = (type === "+") ? 1.1 : 0.9
+  const multiplier = (type === "+") ? 5 : 0.9
   sizeX *= multiplier
   sizeY *= multiplier
 }
